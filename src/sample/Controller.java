e8685b409d6db04bdcfb26997d96c9a0d08e7a16
HttpKiwi <juan.avenia@correounivalle.edu.co>
2020-02-18 00:09:55 -0500

    Initial commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,19 @@
+package sample;
+
+import com.jfoenix.controls.JFXButton;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Controller {
+
+    @FXML
+    private JFXButton btn_login;
+
+    @FXML
+    void log_in(ActionEvent event) {
+
+
+
+    }
+
+}
